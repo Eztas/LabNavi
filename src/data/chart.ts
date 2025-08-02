a436ad21b3b5a5943e402524b1fb666eb9ea1d58
Eztas <nietzsche2669sensei@gmail.com>
2025-08-02 23:47:12 +0900

    add: radarchartに関するデータの型とデータ追加

---
+++ src/data/chart.ts
@@ -0,0 +1,60 @@
+import type { RadarReview } from '.././types/chart'
+
+export const initialRadarReviews: RadarReview[] = [
+  {
+    labId: 'system_control',
+    labName: 'システム制御論研究室',
+    motivation: 4,
+    equipment: 5,
+    longTermGrowth: 4,
+    bottomUp: 3,
+  },
+  {
+    labId: 'system_control',
+    labName: 'システム制御論研究室',
+    motivation: 5,
+    equipment: 4,
+    longTermGrowth: 5,
+    bottomUp: 4,
+  },
+  {
+    labId: 'communication_theory',
+    labName: '通信方式研究室',
+    motivation: 2,
+    equipment: 3,
+    longTermGrowth: 3,
+    bottomUp: 5,
+  },
+  {
+    labId: 'materials_science',
+    labName: '材料科学研究室',
+    motivation: 5,
+    equipment: 5,
+    longTermGrowth: 5,
+    bottomUp: 2,
+  },
+  {
+    labId: 'ai_robotics',
+    labName: 'AIロボティクス研究室',
+    motivation: 3,
+    equipment: 3,
+    longTermGrowth: 2,
+    bottomUp: 1,
+  },
+  {
+    labId: 'human_interface',
+    labName: 'ヒューマンインタフェース研究室',
+    motivation: 5,
+    equipment: 4,
+    longTermGrowth: 4,
+    bottomUp: 5,
+  },
+  {
+    labId: 'materials_science',
+    labName: '材料科学研究室',
+    motivation: 4,
+    equipment: 4,
+    longTermGrowth: 5,
+    bottomUp: 1,
+  },
+];
---
+++ src/types/chart.ts
@@ -0,0 +1,8 @@
+export interface RadarReview {
+  labId: string;
+  labName: string;
+  motivation: number;
+  equipment: number;
+  longTermGrowth: number;
+  bottomUp: number;
+}
