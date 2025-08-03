2ab7b94f99083a5307679a5b725c024fc96b34fc
Eztas <nietzsche2669sensei@gmail.com>
2025-08-04 00:49:16 +0900

    add: vite専用のenvデータの取得機能追加m viteではセキュリティのためにVITE_とついた環境変数のみ受け取る

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVsqY5oT4Z_FsAGj9K4nITJhWHD3qEghk",
-  authDomain: "cosmo-list-app.firebaseapp.com",
-  projectId: "cosmo-list-app",
-  storageBucket: "cosmo-list-app.firebasestorage.app",
-  messagingSenderId: "1006119656840",
-  appId: "1:1006119656840:web:eb01a2473bd08ee034ae3d",
-  measurementId: "G-9BETTN6DFQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
