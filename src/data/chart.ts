82138fab3a17ebc08c6f25db3e4b1cb7e5ce930b
Eztas <nietzsche2669sensei@gmail.com>
2025-08-03 11:05:51 +0900

    change: ラベルの数値の意味をよりポジティブに

--- src/data/chart.ts
+++ src/data/chart.ts
@@ -69,8 +69,24 @@ export const reviewLabels: { [K in keyof RadarAxis]: string } = {
 
 // 各項目の詳細情報（説明文、色など）を定義
 export const reviewLabelDetails: { [K in keyof RadarAxis]: { description: string; thumbColor: string; textColor: string; } } = {
-    motivation: { description: '緩い 1 - 5 高い', thumbColor: 'range-thumb-green', textColor: 'text-green-500' },
-    equipment: { description: '不十分 1 - 5 充実', thumbColor: 'range-thumb-yellow', textColor: 'text-yellow-500' },
-    longTermGrowth: { description: '短期的 1 - 5 長期的', thumbColor: 'range-thumb-purple', textColor: 'text-purple-500' },
-    bottomUp: { description: 'トップダウン 1 - 5 ボトムアップ', thumbColor: 'range-thumb-pink', textColor: 'text-pink-500' },
+    motivation: { 
+        description: '落ち着いた雰囲気 1 - 5 活気のある雰囲気', 
+        thumbColor: 'range-thumb-green', 
+        textColor: 'text-green-500' 
+    },
+    equipment: { 
+        description: '追加の余地あり 1 - 5 設備が充実', 
+        thumbColor: 'range-thumb-yellow', 
+        textColor: 'text-yellow-500' 
+    },
+    longTermGrowth: { 
+        description: 'スピード重視 1 - 5 長期的視点重視', 
+        thumbColor: 'range-thumb-purple', 
+        textColor: 'text-purple-500' 
+    },
+    bottomUp: { 
+        description: '指導体制が明確 1 - 5 学生の裁量が大きい', 
+        thumbColor: 'range-thumb-pink', 
+        textColor: 'text-pink-500' 
+    },
 };
